5777c5249dffbfcbecf1d1f6da7fa6af0082dc3f
Shakib Bhuiyan <78194114+tokir-ahmad629@users.noreply.github.com>
2024-02-14 00:10:07 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+(function ($) {
+    "use strict";
+
+    $(window).on('load', function(){
+        // ------- Prealoder ------
+        $("#preloader").delay(300).fadeOut("slow");
+    });
+
+})(jQuery);
